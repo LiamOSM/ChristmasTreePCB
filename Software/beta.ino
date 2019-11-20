39850aa91a3c2214ddbd6c3f851dfb0aa17f0039
Liam <smolenal@mcmaster.ca>
2019-11-20 16:27:09 -0500

    Create beta.ino

---
+++ Software/beta.ino
@@ -0,0 +1,225 @@
+// This code is basically the same as framework.ino, except
+// I've added the low_power() function, and mode light modes
+
+#include <avr/sleep.h>
+#include <EEPROM.h>
+
+const uint8_t spkrPin = 4; // Speaker
+const uint8_t led5Pin = 3; // LED5 (top of tree)
+
+// shift register pins
+const uint8_t clockPin = 2; // SH_CP
+const uint8_t latchPin = 1; // ST_CP
+const uint8_t dataPin = 0; // DS
+
+// both buttons are on A0
+// note that the "lights" button resets the chip
+const int btnPin = A0; // Buttons
+const uint8_t debounce = 150;
+const int btnMusicThreshold = 902;
+//const int btnLightThreshold = 798;
+//const int btnBothThreshold  = 722;
+const int tolerance = 30;
+
+uint8_t sr = 0b00000000;
+uint8_t mode = 0;
+
+// Some light modes just need to be set once.
+// This flag is used to check if that's happened.
+uint8_t complete = 0;
+
+uint8_t music = 0;
+uint8_t max_state = 6; // last LED mode, rollover to zero next
+
+// modes:
+// 0 = off
+// 1 = all on
+// 2 = flashing
+// 3 = side-to-side
+// 4 = chase
+// 5 = inverted chase
+// 6 = ???
+
+void setup() {
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(led5Pin, OUTPUT);
+  pinMode(spkrPin, OUTPUT);
+
+  // clear LEDs ASAP (SR sets them high on reset)
+  clrLED();
+
+  // on startup, get new state from EEPROM
+  mode = EEPROM.read(0);
+  if (mode > max_state)
+    mode = 0;
+  delay(debounce);
+  // write next state into EEPROM
+  EEPROM.write(0, mode + 1);
+}
+
+void loop() {
+  static bool mode2state = 0;
+  static uint8_t mode3counter = 1;
+  static bool mode4state = 0;
+  static uint8_t mode56counterA = 1;
+  static uint8_t mode56counterB = 2;
+  static unsigned long lastTime = 0;
+
+  if (music) {
+    music = 0;
+    for (int i = 0; i < 100; i ++) {
+      tone(spkrPin, 500);
+      fullLED();
+      delay(50);
+      noTone();
+      clrLED();
+      delay(50);
+    }
+    complete = 0; // return to previous LED pattern
+  }
+
+
+  if (!complete) { // lights need updating
+    switch (mode) {
+      case 0: // idle, 0.2uA current draw
+        clrLED();
+        complete = 1;
+        low_power();
+        // once low_power() is called, the microcontroller
+        // is basically "bricked" until reset is pressed
+        break;
+      case 1: // turn all LEDs on
+        fullLED();
+        complete = 1;
+        break;
+      case 2: // flashing mode
+        if (abs(millis() - lastTime) > 500) { // 0.5 sec passed
+          lastTime = millis();
+          mode2state = !mode2state; // toggle
+          if (mode2state)
+            fullLED();
+          else
+            clrLED();
+        }
+        break;
+      case 3: // side-to-side
+        if (abs(millis() - lastTime) > 80) { // 100ms passed
+          lastTime = millis();
+          if (mode3counter >= 1 && mode3counter <= 9)
+            setLED(mode3counter, 1);
+          else if (mode3counter >= 10 && mode3counter <= 18)
+            setLED(mode3counter - 9, 0);
+          else if (mode3counter >= 19 && mode3counter <= 27)
+            setLED(9 - (mode3counter - 19), 1);
+          else if (mode3counter >= 28 && mode3counter <= 36)
+            setLED(9 - (mode3counter - 28), 0);
+          mode3counter = (mode3counter >= 36) ? 1 : mode3counter + 1;
+        }
+        break;
+      case 4: // twinkle
+        if (abs(millis() - lastTime) > 100) { // 100ms passed
+          lastTime = millis();
+          mode4state = !mode4state;
+          for (uint8_t i = 1; i <= 9; i ++) {
+            setLED(i, (i % 2) ^ mode4state );
+          }
+        }
+        break;
+      case 5: // chase
+        if (abs(millis() - lastTime) > 80) { // 50ms passed
+          lastTime = millis();
+          setLED(mode56counterA, 0);
+          mode56counterA = (mode56counterA >= 11) ? 1 : mode56counterA + 1;
+          mode56counterB = (mode56counterB >= 11) ? 1 : mode56counterB + 1;
+          setLED(mode56counterB, 1);
+        }
+        break;
+      case 6: // inverted chase
+        if (abs(millis() - lastTime) > 80) { // 50ms passed
+          lastTime = millis();
+          setLED(mode56counterA, 1);
+          mode56counterA = (mode56counterA >= 11) ? 1 : mode56counterA + 1;
+          mode56counterB = (mode56counterB >= 11) ? 1 : mode56counterB + 1;
+          setLED(mode56counterB, 0);
+        }
+        break;
+    }
+  }
+
+  int reading = analogRead(A0);
+  // check if music button pressed
+  if (abs(reading - btnMusicThreshold) < tolerance) {
+    music = 1;
+  }
+}
+
+void setLED(int index, bool state) {
+  // the states are all inverted because I'm too lazy to invert
+  // each ternary statement, and I accidentalyl made them all
+  // backwards originally
+  switch (index) {
+    case 1:
+      sr = (!state) ? (sr & 0b11111110) : (sr | 0b00000001);
+      break;
+    case 2:
+      sr = (!state) ? (sr & 0b11111101) : (sr | 0b00000010);
+      break;
+    case 3:
+      sr = (!state) ? (sr & 0b11111011) : (sr | 0b00000100);
+      break;
+    case 4:
+      sr = (!state) ? (sr & 0b11110111) : (sr | 0b00001000);
+      break;
+    case 5:
+      digitalWrite(led5Pin, state ? HIGH : LOW);
+      break;
+    case 6:
+      sr = (!state) ? (sr & 0b11101111) : (sr | 0b00010000);
+      break;
+    case 7:
+      sr = (!state) ? (sr & 0b11011111) : (sr | 0b00100000);
+      break;
+    case 8:
+      sr = (!state) ? (sr & 0b10111111) : (sr | 0b01000000);
+      break;
+    case 9:
+      sr = (!state) ? (sr & 0b01111111) : (sr | 0b10000000);
+      break;
+    default:
+      break;
+  }
+  if (index != 5) {
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, MSBFIRST, sr);
+    digitalWrite(latchPin, HIGH);
+  }
+}
+
+void clrLED() {
+  sr = 0b00000000;
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, sr);
+  digitalWrite(latchPin, HIGH);
+  digitalWrite(led5Pin, LOW);
+}
+
+void fullLED() {
+  sr = 0b11111111;
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, sr);
+  digitalWrite(latchPin, HIGH);
+  digitalWrite(led5Pin, HIGH);
+}
+
+void low_power() {
+  // the chosen sleep mode (SLEEP_MODE_PWR_DOWN)
+  // disables pretty much everything except the
+  // watchdog timer and interrupts, neither of
+  // which are being used in this program
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  ADCSRA &= ~(1 << ADEN); // disable ADC
+  sleep_enable(); // set Sleep Mode Control Register enable bit to 1
+  sleep_cpu(); // enter sleep mode now
+}
